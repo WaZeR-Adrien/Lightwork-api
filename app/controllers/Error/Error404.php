ab3cfe18afd701ff42f054c58b1153b5ae842b9a
WaZeR-Adrien <officielghostfire@gmail.com>
2018-02-07 17:49:56 +0100

    Edit msg 404 error

--- app/controllers/Error/Error404.php
+++ app/controllers/Error/Error404.php
@@ -8,6 +8,6 @@ class Error404 extends Controller
 {
     public static function index()
     {
-        self::_render(404, false,"The URI {$_SERVER['REQUEST_URI']} does not registered");
+        self::_render(404, false,"The URI {$_SERVER['REQUEST_URI']} in {$_SERVER['REQUEST_METHOD']} method does not registered");
     }
 }
