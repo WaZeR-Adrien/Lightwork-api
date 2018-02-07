bff5b3b90972589de5752a701c7573e5d1a7a2e6
WaZeR-Adrien <officielghostfire@gmail.com>
2018-02-07 18:01:02 +0100

    Edit example Auth

--- app/controllers/Auth.php
+++ app/controllers/Auth.php
@@ -7,18 +7,22 @@ class Auth extends Controller
 {
     /**
      * Login and get token if data matches with User
+     * Example of Auth
+     * You can edit this to match with your own database
      */
     public static function check()
     {
-        if (User::check($_POST['email'], $_POST['password'])) {
+        $user = User::check($_POST['email'], $_POST['password']);
+        if ($user) {
             // create token here
             $token = self::_createToken();
             $auth = new \Models\Auth();
-            $auth->token = $token;
-            $auth->date  = time();
+            $auth->user_id = $user->id;
+            $auth->token   = $token;
+            $auth->date    = time();
             $auth->insert();
 
-            self::_render(100, true, 'Successful authentication');
+            self::_render(100, true, 'Successful authentication', ['token' => $token]);
         }
 
         self::_render(101, false, 'Incorrect connection information');
