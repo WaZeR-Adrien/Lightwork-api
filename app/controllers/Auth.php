0f13c2c2b59c276bae402f299b41ed06edd5ad2f
Adrien <adrienm.martineau@gmail.com>
2019-04-21 13:32:20 +0200

    Fix the auth check

--- app/controllers/Auth.php
+++ app/controllers/Auth.php
@@ -16,21 +16,22 @@ class Auth extends Controller
      */
     public static function check(Request $request, Response $response)
     {
-        $user = User::check($request->getBodies()->email, $request->getBodies()->password);
+        $user = User::check($request->getBody()->get("email"), $request->getBody()->get("password"));
         if ($user) {
             // create token here
             $token = Utils::createToken();
             $auth = new \Models\Auth();
-            $auth->setUserId($user->id);
+            $auth->setUser($user);
             $auth->setToken($token);
             $auth->setDate(time());
             $auth->store();
 
-            $response->setData(["token" => $token]);
+            $response->getBody()
+                ->add($token, "token");
 
-            return $response->render("S_PO001")->toJson();
+            return $response->fromApi("S_PO001")->toJson();
         }
 
-        return $response->render("E_A004")->toJson();
+        return $response->fromApi("E_A004")->toJson();
     }
 }
---
+++ app/models/User.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Models;
+
+/**
+ * Class User
+ * @package Models
+ * @table user
+ */
+class User extends Entity
+{
+	/** @var int */
+	private $id;
+
+	/** @var string */
+	private $username;
+
+	/** @var string */
+	private $email;
+
+	/** @var string */
+	private $password;
+
+
+	/**
+	 * @param int id
+	 */
+	public function __construct($id = NULL)
+	{
+		$this->id = $id;
+	}
+
+
+	/**
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+
+	/**
+	 * @param int id
+	 */
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getUsername()
+	{
+		return $this->username;
+	}
+
+
+	/**
+	 * @param string username
+	 */
+	public function setUsername($username)
+	{
+		$this->username = $username;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getEmail()
+	{
+		return $this->email;
+	}
+
+
+	/**
+	 * @param string email
+	 */
+	public function setEmail($email)
+	{
+		$this->email = $email;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getPassword()
+	{
+		return $this->password;
+	}
+
+
+	/**
+	 * @param string password
+	 */
+	public function setPassword($password)
+	{
+		$this->password = $password;
+	}
+
+    /**
+     * Check if email and password correspond
+     * @return array|bool
+     */
+    public function check()
+    {
+        $user = self::findFirst(['email' => $this->email]);
+        if (password_verify($this->password, $user->password)) {
+            return $user;
+        }
+        return false;
+    }
+
+}
