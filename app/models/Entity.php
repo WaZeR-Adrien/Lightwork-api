7347ac841de44dd05aff8eb574cddf58d0f97a22
Adrien Martineau <officielghostfire@gmail.com>
2019-08-30 09:00:18 +0200

    Add fkToObject and remove use Database

--- app/models/Entity.php
+++ app/models/Entity.php
@@ -2,17 +2,42 @@
 
 namespace Models;
 
-use Kernel\Orm\Database;
+use Controllers\Docs;
 use Kernel\Tools\Utils;
 
 /**
- * Class SupportCategory
+ * Class Entity
  * @package Models
  */
 class Entity
 {
-	use Database;
+    /**
+     * Convert all foreign key to object
+     */
+    public function foreignKeyToObject()
+    {
+        foreach ($this as $k => $v) {
+            if (strpos($k, '_id')) {
+                $attr = substr($k, 0, strlen($k) - 3);
+                $attr = Utils::toPascalCase($attr);
 
+                $annotations = Docs::getPhpDoc($this, "get$attr");
+
+                if (!empty($annotations["return"])) {
+                    $class = '\Models\\' . $annotations["return"]["type"];
+                } else {
+                    $class = '\Models\\' . $attr;
+                }
+
+                $class = new $class($v);
+
+                $setter = "set$attr";
+                $this->$setter($class);
+
+                unset($this->$k);
+            }
+        }
+    }
 
 	/**
      * Convert the object to a array and the sub objects to sub arrays
@@ -44,42 +69,5 @@ class Entity
 
 		return $array;
 	}
-
-    /**
-     * Fetch data with id
-     * Example :
-     * $user = new User(10);
-     * $user->fetch(); // Retrieve all data from the user
-     * @param bool $recursive
-     */
-    public function fetch($recursive = false)
-    {
-        $class = get_class($this);
-
-        if ($this->getId() != null) {
-            $reflect = new \ReflectionObject($this);
-            $obj = $class::getById($this->getId());
-
-            foreach ($reflect->getProperties() as $property) {
-                if (!$property->isStatic()) {
-
-                    $setter = "set" . Utils::toPascalCase($property->getName());
-                    $getter = "get" . Utils::toPascalCase($property->getName());
-
-                    // If the value is an object and the recursive mode is true :
-                    // Fetch all sub data
-                    if ($recursive && is_object($obj->$getter())) {
-                        $obj->$getter()->fetch(true);
-
-                    }
-
-                    $this->$setter( $obj->$getter() );
-
-                }
-            }
-        }
-
-        return $this;
-    }
 
 }
