584103ae3ac86c1b079c733015502d0c55975421
Adrien Martineau <officielghostfire@gmail.com>
2019-10-03 22:58:54 +0200

    Add codes

--- kernel/Api/ApiErrorCode.php
+++ kernel/Api/ApiErrorCode.php
@@ -5,6 +5,18 @@ use Kernel\Http\StatusCode;
 
 class ApiErrorCode
 {
+    public const PAGE001 = [
+        "code" => "PAGE001",
+        "status" => StatusCode::HTTP_BAD_REQUEST,
+        "message" => "Incorrect information of pageable. The length must be int superior to 0, :key given."
+    ];
+
+    public const PAGE002 = [
+        "code" => "PAGE002",
+        "status" => StatusCode::HTTP_BAD_REQUEST,
+        "message" => "Incorrect information of pageable. The current page must be int superior to 0, :key given."
+    ];
+
     public const NF404 = [
         "code" => "NF404",
         "status" => StatusCode::HTTP_NOT_FOUND,
